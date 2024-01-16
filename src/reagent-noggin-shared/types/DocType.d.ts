53a7a57bac69cc5a8804b254e96f6df4c3d6f216
Timothy J. Aveni <tja@berkeley.edu>
2024-01-15 17:37:35 -0800

    Rename 'parameter' -> 'variable', except in data

--- src/reagent-noggin-shared/types/DocType.d.ts
+++ src/reagent-noggin-shared/types/DocType.d.ts
@@ -1,26 +1,26 @@
 import * as Y from 'yjs';
 
-type DocumentBaseParameter = {
+type DocumentBaseVariable = {
   name: string;
 };
 
-export interface DocumentTextParameter extends DocumentBaseParameter {
+export interface DocumentTextVariable extends DocumentBaseVariable {
   type: 'text';
   maxLength: number;
   defaultValue: string;
 }
 
-export interface DocumentImageParameter extends DocumentBaseParameter {
+export interface DocumentImageVariable extends DocumentBaseVariable {
   type: 'image';
   openAI_detail: 'low' | 'high' | 'auto'; // openai-centric for now. also maybe in the future we do our own scaling in the shim?
 }
 
-export type DocumentParameter = DocumentTextParameter | DocumentImageParameter;
-type _DPTypeCheck = DocumentParameter['type'];
+export type DocumentVariable = DocumentTextVariable | DocumentImageVariable;
+type _DPTypeCheck = DocumentVariable['type'];
 
 export type DocType = {
   modelInputs: Record<string, Y.XmlText | any>;
-  documentParameters: Record<string, Y.Map<DocumentParameter>>;
+  documentParameters: Record<string, Y.Map<DocumentVariable>>;
   // using Object.keys on documentParameters doesn't trigger a rerender on the index component, so we also keep a list of IDs so that the `push` gets noticed by the rerender logic...
   documentParameterIdsByDocument: Record<string, string[]>;
   nogginOptions: {
@@ -33,7 +33,7 @@ export type DocType = {
 
 export type JSONDocType = {
   modelInputs: Record<string, any>;
-  documentParameters: Record<string, DocumentParameter>;
+  documentParameters: Record<string, DocumentVariable>;
   documentParameterIdsByDocument: Record<string, string[]>;
   nogginOptions: {
     chosenOutputFormatKey: string;
