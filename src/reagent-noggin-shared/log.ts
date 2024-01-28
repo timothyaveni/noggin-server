86cc50661a042757df5141ecbb9beb277cee8139
Timothy J. Aveni <tja@berkeley.edu>
2024-01-28 12:30:17 -0800

    Patch logging enums into code (prisma import don't work)

--- src/reagent-noggin-shared/log.ts
+++ src/reagent-noggin-shared/log.ts
@@ -1,4 +1,17 @@
-import { NogginRunLogEntryStage, NogginRunLogLevel } from '@prisma/client';
+// ugh, still having that prisma enum info. will need to keep this in sync
+
+type NogginRunLogLevel = 'debug' | 'info' | 'warn' | 'error';
+
+type NogginRunLogEntryStage =
+  | 'request'
+  | 'authenticate'
+  | 'process_parameters'
+  | 'anticipate_cost'
+  | 'run_model'
+  | 'calculate_cost'
+  | 'postprocess'
+  | 'deliver'
+  | 'other';
 
 export type LogEntry = {
   level: NogginRunLogLevel;
