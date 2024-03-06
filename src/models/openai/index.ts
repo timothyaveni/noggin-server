223cc2fd76eba517f8016e829cce51e18b3571ae
Timothy J. Aveni <tja@berkeley.edu>
2024-03-05 22:11:05 -0800

    Add gpt-3.5-turbo-0125

--- src/models/openai/index.ts
+++ src/models/openai/index.ts
@@ -1,4 +1,5 @@
 import { ModelExports } from '../index.js';
+import { streamResponse as streamResponse_gpt35Turbo0125 } from './gpt-3.5-turbo-0125.js';
 import { streamResponse as streamResponse_gpt35Turbo1106 } from './gpt-3.5-turbo-1106.js';
 import { streamResponse as streamResponse_gpt41106Preview } from './gpt-4-1106-preview.js';
 import { streamResponse as streamResponse_gpt4VisionPreview } from './gpt-4-vision-preview.js';
@@ -8,6 +9,10 @@ export default function openaiIndex(modelName: string): ModelExports {
     case 'gpt-3.5-turbo-1106':
       return {
         streamResponse: streamResponse_gpt35Turbo1106,
+      };
+    case 'gpt-3.5-turbo-0125':
+      return {
+        streamResponse: streamResponse_gpt35Turbo0125,
       };
     case 'gpt-4-1106-preview':
       return {
